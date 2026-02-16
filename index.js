d4e1844e673b4760a3bef89066507d3acf90460b
Himanshu Thakur <96857622+tHimanshu2003@users.noreply.github.com>
2026-02-16 21:12:01 +0530

    Update CORS origin to allow all origins

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-hub-gray.vercel.app/'],
+    origin: ['*'],
     credentials: true 
 }))
 
