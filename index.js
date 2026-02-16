b6509da16792c297f0ccc8199ba597e4a76ee390
Himanshu Thakur <96857622+tHimanshu2003@users.noreply.github.com>
2026-02-16 22:03:54 +0530

    Update CORS configuration with allowed origins

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ require('dotenv').config()
 
 // middleware
 app.use(express.json());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://book-hub-gray.vercel.app",
+    "*"
+];
+
 app.use(cors({
-    origin: ['*'],
-    credentials: true 
-}))
+  origin: function (origin, callback) {
+    // allow requests with no origin (mobile apps, postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed: " + origin));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 // routes
 const bookRoutes = require('./src/books/book.route');
